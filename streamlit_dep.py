33e0d945352684e45377fb16eb39d44d30b10509
Dr. Stephanie Velez <stephanievelez1986@gmail.com>
2024-12-13 13:18:14 -0600

    Update streamlit_dep.py

--- streamlit_dep.py
+++ streamlit_dep.py
@@ -12,13 +12,14 @@ from langchain.agents import AgentExecutor
 
 with open("test", "rb") as fp:   # Unpickling
     b = pickle.load(fp)
+
 df_document = b
 
 
 st.title("💬 Pharmacogenomics Bot")
 st.write(
     "This is a simple chatbot that is trained on documents that were queried from the CPIC database."
-    "It is trained on CYP2C19 genes so far, and can understand phenotypes such as *17/*17 or ultra-rapid metabolizer."
+    "It is trained on CYP2C19 genes so far, and can understand phenotypes such as *17/*17 or ultra-rapid metabolizer." 
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
@@ -37,52 +38,60 @@ else:
             documents=df_document,
             collection_name="my_collection",
             embedding=embeddings,
-            persist_directory="./chroma_db"  # In-memory mode
-        )
+            persist_directory="./chroma_db",  # Where to save data locally, remove if not neccesary
+        )  # In-memory mode
 
     vector_store = st.session_state.vector_store
+    vector_store.persist()
     # Create an OpenAI client.
-    # client = OpenAI(api_key=openai_api_key)
-    #
-    # # Create a session state variable to store the chat messages. This ensures that the
-    # # messages persist across reruns.
-    # if "messages" not in st.session_state:
-    #     st.session_state.messages = []
-    #
-    # # Display the existing chat messages via `st.chat_message`.
-    # for message in st.session_state.messages:
-    #     with st.chat_message(message["role"]):
-    #         st.markdown(message["content"])
-    #
-    # # Create a chat input field to allow the user to enter a message. This will display
-    # # automatically at the bottom of the page.
-    # if prompt := st.chat_input("How can I help? 🤖 "):
-    #
-    #     # Store and display the current prompt.
-    #     st.session_state.messages.append({"role": "user", "content": prompt})
-    #     with st.chat_message("user"):
-    #         st.markdown(prompt)
-    #
-    #     # Generate a response using the OpenAI API.
-    #     stream = []
-    #     for m in st.session_state.messages:
-    #         stream.append(generate_response(openai_api_key, m["content"]))
-    #
-    #     # Stream the response to the chat using `st.write_stream`, then store it in
-    #     # session state.
-    #     with st.chat_message("assistant"):
-    #         response = st.write_stream(stream)
-    #     st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 # Query Input
     query = st.text_input("Ask your question:")
-
     if st.button("Submit"):
         if query:
         # Set up QA chain
-            qa_chain = RetrievalQA.from_chain_type(
-                llm=OpenAI(openai_api_key=openai_api_key),
+            llm = OpenAI(openai_api_key=openai_api_key, temperature = 0.0)
+            qa = RetrievalQA.from_chain_type(
+                llm=llm,
                 retriever=vector_store.as_retriever()
             )
-            answer = qa_chain.run(query)
-            st.write("Answer:", answer)
+
+            conversational_memory = ConversationBufferWindowMemory(
+                memory_key='chat_history',
+                k=4,  # Number of messages stored in memory
+                return_messages=True  # Must return the messages in the response.
+            )
+
+            tools = [
+                Tool(
+                    name='CPIC Bot',
+                    func=qa.invoke,
+                    description=(
+                    """use this tool when answering how to dose a medication given a phenotype and or a diplotype"""
+                    )
+                )
+            ]
+
+            prompt = hub.pull("hwchase17/react-chat")
+            agent = create_react_agent(
+                tools=tools,
+                llm=llm,
+                prompt=prompt,
+            )
+
+            agent_executor = AgentExecutor(agent=agent,
+                                       tools=tools,
+                                       verbose=True,
+                                       memory=conversational_memory,
+                                       max_iterations=30,
+                                       max_execution_time=600,
+                                       handle_parsing_errors=True
+                                       )
+
+            answer = agent_executor.invoke({"input": query})
+
+
+
+            #answer = qa_chain.invoke({"input": query})
+            st.write("Answer:", answer['output'])
